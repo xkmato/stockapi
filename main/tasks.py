401845e30f7795906a9f45fd81ef3dcb66cdff32
Kenneth <kbonky@gmail.com>
2019-04-05 09:16:42 +0300

    introducing celery

---
+++ main/tasks.py
@@ -0,0 +1,10 @@
+import logging
+
+from celery import shared_task
+
+logger = logging.getLogger(__name__)
+
+
+@shared_task
+def collect_price_updates():
+    logger.info("Supposed to periodically collect price update info from external resource")
